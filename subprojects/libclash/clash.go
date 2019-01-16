d3bec01f14a613edfa5229e5f3a72fc9db05b0c1
Balthild <ibalthild@gmail.com>
2019-01-16 22:01:39 +0800

    Update clash

--- subprojects/libclash/clash.go
+++ subprojects/libclash/clash.go
@@ -3,22 +3,13 @@ package main
 import (
 	"C"
 
-	"github.com/Dreamacro/clash/config"
 	"github.com/Dreamacro/clash/constant"
 	"github.com/Dreamacro/clash/hub"
-	"github.com/Dreamacro/clash/proxy"
-	"github.com/Dreamacro/clash/tunnel"
 )
 
 //export clash_run
 func clash_run() *C.char {
-	tunnel.Instance().Run()
-	proxy.Instance().Run()
-	hub.Run()
-
-	config.Init()
-	err := config.Instance().Parse()
-	if err != nil {
+	if err := hub.Parse(); err != nil {
 		return C.CString(err.Error())
 	}
 
@@ -27,10 +18,10 @@ func clash_run() *C.char {
 
 //export clash_update_all_config
 func clash_update_all_config() *C.char {
-	err := config.Instance().Parse()
-	if err != nil {
+	if err := hub.Parse(); err != nil {
 		return C.CString(err.Error())
 	}
+
 	return C.CString("success")
 }
 
